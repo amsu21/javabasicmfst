78780ef1b9c39c6a62897a5d5bc74dc3faa762ec
amsu21 <aswb4@hotmail.com>
2024-07-18 17:59:11 -0400

    Basic Java

---
+++ basicjava/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Person p = new Person("Amsu Warner");
+        System.out.println("Hello " + p.getName());
+    }
+}
